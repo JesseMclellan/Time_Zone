906e15628ae7841651f829bd72e3bf7888bf6638
Jesse <jesse_mclellan@yahoo.com>
2017-07-19 16:46:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  date = new Date();
+  timeZone = 0;
+  lastTimeZoneSelected = null;
+
+  localOffset(){
+    return this.date.getTimezoneOffset() * 60000 * this.timeZone;
+  }
+  changeTime(tz) {
+    this.date = new Date();
+    if(tz=="PST"){
+      this.timeZone = 0;
+    }
+    else if(tz=="MST") {
+      this.timeZone = 1;
+    }
+    else if(tz=="CST") {
+     this.timeZone = 2;
+    }
+    else if(tz=="EST") {
+     this.timeZone = 3;
+    };
+    this.lastTimeZoneSelected = tz;
+    this.date.setHours(this.date.getHours() + this.timeZone);
+  }
 }
